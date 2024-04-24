ecdb14c4f622177762274265ab84da636e15a383
Kirill Krasnovskiy <kirusha2435896@gmail.com>
2024-04-24 22:47:11 +0300

    modified:   getPdqlGroupsHierarchy.py

--- getPdqlGroupsHierarchy.py
+++ getPdqlGroupsHierarchy.py
@@ -10,7 +10,7 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-bearerToken = '48DEF6BAB67E7E592E72C976E3548C31D765B01635F7C390C0D1D0E9F867CD3A'
+bearerToken = 'E93F95E0BC0CF5C6BCC9AD82CDB75A19ADCCFE184F2AD9ED29C2A8B9B5C8EDA8'
 rootUrl = 'https://10.2.183.20/api/assets_temporal_readmodel/v1/stored_queries/folders/queries/'
 
 currentDirectory = os.path.dirname(os.path.abspath(__file__))                           #текущая директория
@@ -28,3 +28,36 @@ def downloadPdqlGroupsData(pdqlGroupsJsonFile):
 
     with open (pdqlGroupsJsonFile, 'w', encoding='utf-8') as pdqlGroupsFile:
         json.dump(response.json(), pdqlGroupsFile, ensure_ascii=False)
+
+#ЗАПРОСЫ PDQL || функция поиска id группы по displayName в файле groupsOfQuerries.json
+def getPdqlGroupId(groupsData, displayName):
+    if not groupsData:
+        print('Не было найдено группы с именем: ' + displayName)
+        return None
+
+    for group in groupsData:
+        if group["displayName"] == displayName:
+            return group["id"]
+        
+        if 'children' in group:
+            result = getPdqlGroupId(group, displayName)
+            if result:
+                return result
+            
+
+downloadPdqlGroupsData(pdqlGroupsJsonFile)
+
+
+
+with open(pdqlGroupsJsonFile, 'r', encoding='utf-8') as pdqlGroupsFile:
+    groupsData = json.load(pdqlGroupsFile)["nodes"]
+
+
+
+print(json.dumps(groupsData, indent=4, sort_keys=True, ensure_ascii=False))
+
+
+
+
+
+print(getPdqlGroupId(groupsData, 'Name_of_group'))
